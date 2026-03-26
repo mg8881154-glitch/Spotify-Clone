297200eb142700e810c995efe9e3279a2b747ca5
Muskan Gupta <mg8881154@gmail.com>
2026-03-26 18:48:56 +0530

    Add home navigation to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Sidebar = () => {
+  const navigate = useNavigate()
   return (
     <div className='w-[25%] h-full gap-2 text-white hidden lg:flex flex-col'>
       
       <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
         
         {/* Home */}
-        <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+        <div onClick={() => navigate('/')} className='flex items-center gap-3 pl-8 cursor-pointer'>
           <img className='w-6' src={assets.home_icon} alt="home" />
           <p className='font-bold'>Home</p>
         </div>
